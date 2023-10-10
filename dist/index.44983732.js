1f0f990edc5ff7f47d9d94b5954ef27fc5336720
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-10 20:58:44 +0300

    init: start project

---
+++ dist/index.44983732.js
@@ -0,0 +1,24 @@
+//function changeColor() {
+//let container = document.querySelector('#container');
+//container.addEventListener('click', function(event) {
+//let target = event.target;
+//if (target.className == 'btn') {
+//console.log('No');
+//}
+//}); 
+//};
+//document.addEventListener('DOMContentLoaded', changeColor);
+//let button = document.querySelector('.btn');
+//function showConsole() {
+//console.log('No');
+//};
+//button.addEventListener('click', showConsole);
+function showMessage() {
+    let button = document.querySelector(".btn");
+    button.addEventListener("click", function() {
+        console.log("Click");
+    });
+}
+document.addEventListener("DOMContentLoaded", showMessage);
+
+//# sourceMappingURL=index.44983732.js.map
