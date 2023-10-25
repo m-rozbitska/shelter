53ac7f9115ce9a6045e444c9718a89e815d0da7b
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-25 21:52:16 +0300

    fix code

--- src/script.js
+++ src/script.js
@@ -4,7 +4,42 @@ document.addEventListener('DOMContentLoaded', () => {
 		infinite: true,
 		speed: 300,
 		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToScroll: 3,
+		responsive: [
+		  {
+			 breakpoint: 1024,
+			 settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			 }
+		  },
+		  {
+			 breakpoint: 600,
+			 settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			 }
+		  },
+		  {
+			 breakpoint: 480,
+			 settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			 }
+		  }
+		]
+	 });
+
+	 $('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		// centerMode: true,
+		// centerPadding: '60px',
 		// responsive: [
 		//   {
 		// 	 breakpoint: 1024,
@@ -29,9 +64,6 @@ document.addEventListener('DOMContentLoaded', () => {
 		// 		slidesToScroll: 1
 		// 	 }
 		//   }
-		//   // You can unslick at a given breakpoint now by adding:
-		//   // settings: "unslick"
-		//   // instead of a settings object
 		// ]
 	 });
 })
