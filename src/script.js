0b4db5e68805e50793eef8fb49f1f6f6fa6f70bc
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-26 22:05:31 +0300

    fix styles of footer

--- src/script.js
+++ src/script.js
@@ -38,32 +38,33 @@ document.addEventListener('DOMContentLoaded', () => {
 		speed: 300,
 		slidesToShow: 3,
 		slidesToScroll: 3,
-		// centerMode: true,
-		// centerPadding: '60px',
-		// responsive: [
-		//   {
-		// 	 breakpoint: 1024,
-		// 	 settings: {
-		// 		slidesToShow: 3,
-		// 		slidesToScroll: 3,
-		// 		infinite: true,
-		// 		dots: true
-		// 	 }
-		//   },
-		//   {
-		// 	 breakpoint: 600,
-		// 	 settings: {
-		// 		slidesToShow: 2,
-		// 		slidesToScroll: 2
-		// 	 }
-		//   },
-		//   {
-		// 	 breakpoint: 480,
-		// 	 settings: {
-		// 		slidesToShow: 1,
-		// 		slidesToScroll: 1
-		// 	 }
-		//   }
-		// ]
+		adaptiveHeight: true,
+		variableWidth: true,
+	
+		responsive: [
+		  {
+			 breakpoint: 1024,
+			 settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			 }
+		  },
+		  {
+			 breakpoint: 768,
+			 settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			 }
+		  },
+		  {
+			 breakpoint: 380,
+			 settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			 }
+		  }
+		]
 	 });
 })
