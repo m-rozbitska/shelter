ee7f28aebd1e88f8de38fce8c5136cbc112c4352
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-27 23:09:56 +0300

    add js burger

--- src/script.js
+++ src/script.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+	const menu = document.querySelector('.menu__body');
+	const menuBtn = document.querySelector('.menu__icon');
+	const body = document.body;
+	const menuLinks = document.querySelectorAll('.menu__link');
+
+
+		menuBtn.addEventListener('click', () => {
+			menu.classList.toggle('active');
+			menuBtn.classList.toggle('active');
+			body.classList.toggle('lock');
+		});
+
+		menuLinks.forEach(link => {
+			link.addEventListener('click', () => {
+				menu.classList.remove('active');
+				menuBtn.classList.remove('active');
+				body.classList.remove('lock');
+			});
+		});
+		
 	$('.slider__pets').slick({
 		dots: true,
 		infinite: true,
