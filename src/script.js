1d527431b3ef420a49698ff087bf454c1365cfc8
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-30 23:03:38 +0200

    feat: add adaptive to page pets

--- src/script.js
+++ src/script.js
@@ -26,6 +26,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		speed: 300,
 		slidesToShow: 4,
 		slidesToScroll: 3,
+		adaptiveHeight: true,
+		variableWidth: true,
 		responsive: [
 		  {
 			 breakpoint: 1024,
@@ -37,14 +39,14 @@ document.addEventListener('DOMContentLoaded', () => {
 			 }
 		  },
 		  {
-			 breakpoint: 600,
+			 breakpoint: 768,
 			 settings: {
 				slidesToShow: 2,
 				slidesToScroll: 2
 			 }
 		  },
 		  {
-			 breakpoint: 480,
+			 breakpoint: 320,
 			 settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1
