96f339d7673ea58ec53406679ac60fe2d53adc70
Mariia Rozbitska <melissa180lily@gmail.com>
2023-11-26 20:05:24 +0200

    fix code

--- src/script.js
+++ src/script.js
@@ -6,7 +6,6 @@ import scarletImg from './images/pets-scarlet.png';
 import timmyImg from './images/pets-timmy.png';
 import freddieImg from './images/pets-freddie.png';
 import charlyImg from './images/pets-charly.png';
-import paginationDisable from './images/paginator-inactive.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		});
 
-	 $('.slider').slick({
+	$('.slider').slick({
 		dots: true,
 		infinite: true,
 		speed: 300,
@@ -39,43 +38,43 @@ document.addEventListener('DOMContentLoaded', () => {
 		adaptiveHeight: true,
 		variableWidth: true,
 		responsive: [
-		  {
-			 breakpoint: 1024,
-			 settings: {
+		{
+			breakpoint: 1024,
+			settings: {
 				slidesToShow: 3,
 				slidesToScroll: 3,
 				infinite: true,
 				dots: true
-			 }
-		  },
-		  {
-			 breakpoint: 768,
-			 settings: {
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
 				slidesToShow: 2,
 				slidesToScroll: 2
-			 }
-		  },
-		  {
-			 breakpoint: 380,
-			 settings: {
+			}
+		},
+		{
+			breakpoint: 380,
+			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1
-			 }
-		  }
+			}
+		}
 		]
-	 });
+	});
 })
 
 document.addEventListener('DOMContentLoaded', function () {
 	const openModalLinks = document.querySelectorAll('.slider__btn');
 
 	openModalLinks.forEach(link => {
-		 link.addEventListener('click', function (e) {
-			  e.preventDefault();
-			  const dataIndex = this.getAttribute('data-index');
-			  loadData(dataIndex);
-			  openModal();
-		 });
+		link.addEventListener('click', function (e) {
+			e.preventDefault();
+			const dataIndex = this.getAttribute('data-index');
+			loadData(dataIndex);
+			openModal();
+		});
 	});
 
 	
@@ -105,93 +104,93 @@ document.addEventListener('DOMContentLoaded', function () {
 		"breed": "Shih tzu",
 		"description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
 		"age": "1 month",
-	 };
+	};
 	
-	 const woodyData = {
+	const woodyData = {
 		"img": woodyImg,
 		"name": "Woody",
 		"type": "Dog",
 		"breed": "Golden Retriever",
 		"description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
 		"age": "3 years 6 months",
-	 };
+	};
 	
-	 const scarlettData = {
+	const scarlettData = {
 		"img": scarletImg,
 		"name": "Scarlett",
 		"type": "Dog",
 		"breed": "Jack Russell Terrier",
 		"description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
 		"age": "3 months",
-	 };
+	};
 	
-	 const timmyData = {
+	const timmyData = {
 		"img": timmyImg,
 		"name": "Timmy",
 		"type": "Cat",
 		"breed": "British Shorthair",
 		"description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
 		"age": "2 years 3 months",
-	 };
-	 
-	 const freddieData = {
+	};
+	
+	const freddieData = {
 		"img": freddieImg,
 		"name": "Freddie",
 		"type": "Cat",
 		"breed": "British Shorthair",
 		"description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
 		"age": "2 months",
-	 };
+	};
 	
-	 const charlyData = {
+	const charlyData = {
 		"img": charlyImg,
 		"name": "Charly",
 		"type": "Dog",
 		"breed": "Jack Russell Terrier",
 		"description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
 		"age": "8 years",
-	 };
+	};
 
 	function loadData(index) {
-		 const data = getDataByIndex(index);
+		const data = getDataByIndex(index);
 
-		 const modalImg = document.querySelector('#modalImg');
-		 const modalTitle = document.querySelector('#modalTitle');
-		 const modalType = document.querySelector('#modalType');
-		 const modalBreed = document.querySelector('#modalBreed');
-		 const modalAge = document.querySelector('#modalAge');
-		 const modalDescr = document.querySelector('#modalDescr');
+		const modalImg = document.querySelector('#modalImg');
+		const modalTitle = document.querySelector('#modalTitle');
+		const modalType = document.querySelector('#modalType');
+		const modalBreed = document.querySelector('#modalBreed');
+		const modalAge = document.querySelector('#modalAge');
+		const modalDescr = document.querySelector('#modalDescr');
 
-		 modalImg.src = data.img;
-		 modalTitle.innerText = data.name;
-		 modalType.innerText = data.type;
-		 modalBreed.innerText = data.breed;
-		 modalAge.innerText = data.age;
-		 modalDescr.innerText = data.description;
+		modalImg.src = data.img;
+		modalTitle.innerText = data.name;
+		modalType.innerText = data.type;
+		modalBreed.innerText = data.breed;
+		modalAge.innerText = data.age;
+		modalDescr.innerText = data.description;
 	}
 
 	function getDataByIndex(index) {
-		 const dataObjects = [
-			  petsData,
-			  jenniferData,
-			  woodyData,
-			  sophiaData,
-			  timmyData,
-			  charlyData,
-			  scarlettData,
-			  freddieData
-		 ];
+		const dataObjects = [
+			petsData,
+			jenniferData,
+			woodyData,
+			sophiaData,
+			timmyData,
+			charlyData,
+			scarlettData,
+			freddieData
+		];
 
-		 if (index >= 0 && index < dataObjects.length) {
-			  return dataObjects[index];
-		 } else {
-			  return {};
-		 }
+		if (index >= 0 && index < dataObjects.length) {
+			return dataObjects[index];
+		} else {
+			return {};
+		}
 	}
 
 	function openModal() {
-		 const modal = document.getElementById('modal');
-		 modal.style.display = 'flex';
+		const modal = document.getElementById('modal');
+		modal.style.display = 'flex';
 	}
 
 	const closeModalBtn = document.querySelector('#closeBtnModal');
@@ -201,13 +200,13 @@ document.addEventListener('DOMContentLoaded', function () {
 	modal.addEventListener('click', outsideClick);
 
 	function closeModal() {
-		 modal.style.display = 'none';
+		modal.style.display = 'none';
 	}
 
 	function outsideClick(e) {
-		 if (e.target === modal) {
-			  closeModal();
-		 }
+		if (e.target === modal) {
+			closeModal();
+		}
 	}
 });
 
@@ -215,106 +214,106 @@ document.addEventListener('DOMContentLoaded', function () {
 	const sliderContainer = document.querySelector('.slider__pets');
 	const itemsPerPage = 8; // Кількість елементів на сторінці
 	let currentPage = 1;
- 
+
 	const totalItems = sliderContainer.querySelectorAll('.slider__item').length;
 	const totalPages = Math.ceil(totalItems / itemsPerPage);
- 
+
 	// Оновлення стану пагінації
 	function updatePagination() {
-	  const paginationBtns = document.querySelectorAll('.pagination__btn');
-	  paginationBtns.forEach((btn, index) => {
-		 if (index === 2) {
+	const paginationBtns = document.querySelectorAll('.pagination__btn');
+	paginationBtns.forEach((btn, index) => {
+		if (index === 2) {
 			btn.textContent = currentPage;
-		 } else if (index === 0) {
+		} else if (index === 0) {
 			btn.disabled = currentPage === 1;
-		 } else if (index === 1) {
+		} else if (index === 1) {
 			btn.disabled = currentPage === 1;
-		 } else if (index === 3) {
+		} else if (index === 3) {
 			btn.disabled = currentPage === totalPages;
-		 } else if (index === 4) {
+		} else if (index === 4) {
 			btn.disabled = currentPage === totalPages;
-		 }
-	  });
+		}
+	});
 	}
- 
+
 	// Перехід до вказаної сторінки
 	function goToPage(page) {
-	  currentPage = page;
-	  const startIndex = (currentPage - 1) * itemsPerPage;
-	  const endIndex = startIndex + itemsPerPage;
- 
-	  const items = sliderContainer.querySelectorAll('.slider__item');
-	  items.forEach((item, index) => {
-		 item.style.display = index >= startIndex && index < endIndex ? 'flex' : 'none';
-	  });
- 
-	  updatePagination();
+	currentPage = page;
+	const startIndex = (currentPage - 1) * itemsPerPage;
+	const endIndex = startIndex + itemsPerPage;
+
+	const items = sliderContainer.querySelectorAll('.slider__item');
+		items.forEach((item, index) => {
+		item.style.display = index >= startIndex && index < endIndex ? 'flex' : 'none';
+	});
+
+	updatePagination();
 	}
- 
+
 	// Обробка кліку на кнопку пагінації
 	function handlePaginationClick(e) {
-	  const action = e.target.dataset.action;
- 
-	  if (action === 'prev' && currentPage > 1) {
-		 goToPage(currentPage - 1);
-	  } else if (action === 'next' && currentPage < totalPages) {
-		 goToPage(currentPage + 1);
-	  } else if (action === 'first') {
-		 goToPage(1);
-	  } else if (action === 'last') {
-		 goToPage(totalPages);
-	  }
+		const action = e.target.dataset.action;
+
+	if (action === 'prev' && currentPage > 1) {
+	goToPage(currentPage - 1);
+	} else if (action === 'next' && currentPage < totalPages) {
+	goToPage(currentPage + 1);
+	} else if (action === 'first') {
+	goToPage(1);
+	} else if (action === 'last') {
+	goToPage(totalPages);
+		}
 	}
- 
+
 	// Створення елементів пагінації
 	function createPagination() {
-	  const paginationContainer = document.createElement('div');
-	  paginationContainer.classList.add('btns__container');
- 
-	  const prevBtn = document.createElement('button');
-	  prevBtn.classList.add('pagination__btn');
-	  prevBtn.textContent = '<<';
+	const paginationContainer = document.createElement('div');
+	paginationContainer.classList.add('btns__container');
+
+	const prevBtn = document.createElement('button');
+	prevBtn.classList.add('pagination__btn');
+	prevBtn.textContent = '<<';
 	   //привязати js code (logic) до кнопок, що зверстані в html
 		// document.querySelector()
-	  prevBtn.addEventListener('click', handlePaginationClick);
-	  prevBtn.dataset.action = 'prev';
+	prevBtn.addEventListener('click', handlePaginationClick);
+	prevBtn.dataset.action = 'prev';
 
-	  const firstBtn = document.createElement('button');
-	  firstBtn.classList.add('pagination__btn');
-	  firstBtn.textContent = '<';
-	  firstBtn.addEventListener('click', handlePaginationClick);
-	  firstBtn.dataset.action = 'first';
- 
-	  const currentPageBtn = document.createElement('button');
-	  currentPageBtn.classList.add('pagination__btn', 'current-page');
-	  currentPageBtn.textContent = currentPage;
- 
-	  const lastBtn = document.createElement('button');
-	  lastBtn.classList.add('pagination__btn');
-	  lastBtn.textContent = '>';
-	  lastBtn.addEventListener('click', handlePaginationClick);
-	  lastBtn.dataset.action = 'last';
- 
-	  const nextBtn = document.createElement('button');
-	  nextBtn.classList.add('pagination__btn');
-	  nextBtn.textContent = '>>';
-	  nextBtn.addEventListener('click', handlePaginationClick);
-	  nextBtn.dataset.action = 'next';
- 
-	  paginationContainer.appendChild(prevBtn);
-	  paginationContainer.appendChild(firstBtn);
-	  paginationContainer.appendChild(currentPageBtn);
-	  paginationContainer.appendChild(lastBtn);
-	  paginationContainer.appendChild(nextBtn);
- 
-	  return paginationContainer;
+	const firstBtn = document.createElement('button');
+	firstBtn.classList.add('pagination__btn');
+	firstBtn.textContent = '<';
+	firstBtn.addEventListener('click', handlePaginationClick);
+	firstBtn.dataset.action = 'first';
+
+	const currentPageBtn = document.createElement('button');
+	currentPageBtn.classList.add('pagination__btn', 'current-page');
+	currentPageBtn.textContent = currentPage;
+
+	const lastBtn = document.createElement('button');
+	lastBtn.classList.add('pagination__btn');
+	lastBtn.textContent = '>';
+	lastBtn.addEventListener('click', handlePaginationClick);
+	lastBtn.dataset.action = 'last';
+
+	const nextBtn = document.createElement('button');
+	nextBtn.classList.add('pagination__btn');
+	nextBtn.textContent = '>>';
+	nextBtn.addEventListener('click', handlePaginationClick);
+	nextBtn.dataset.action = 'next';
+
+	paginationContainer.appendChild(prevBtn);
+	paginationContainer.appendChild(firstBtn);
+	paginationContainer.appendChild(currentPageBtn);
+	paginationContainer.appendChild(lastBtn);
+	paginationContainer.appendChild(nextBtn);
+
+	return paginationContainer;
 	}
- 
+
 	// Додавання пагінації до сторінки
 	const pagination = createPagination();
 	sliderContainer.appendChild(pagination);
- 
+
 	// Початкове завантаження першої сторінки
 	goToPage(currentPage);
- });
- 
+});
+
