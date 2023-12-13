c2ad6bc4d2f4e05212fd13c83b1b9f297f93ccd1
Mariia Rozbitska <melissa180lily@gmail.com>
2023-12-13 14:10:58 +0200

    fix styles

--- src/script.js
+++ src/script.js
@@ -34,29 +34,38 @@ document.addEventListener('DOMContentLoaded', () => {
 		infinite: true,
 		speed: 300,
 		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToScroll: 1,
 		adaptiveHeight: true,
 		variableWidth: true,
+		centerMode: true,
+		centerPadding: '30px',
 		responsive: [
 		{
 			breakpoint: 1024,
 			settings: {
 				slidesToShow: 3,
-				slidesToScroll: 3,
+				slidesToScroll: 1,
 				infinite: true,
-				dots: true
+				dots: true,
+				adaptiveHeight: true,
+				variableWidth: true,
+				centerMode: true,
+				centerPadding: '30px',
 			}
 		},
 		{
 			breakpoint: 768,
 			settings: {
 				slidesToShow: 2,
-				slidesToScroll: 2
+				slidesToScroll: 1,
+				centerMode: true,
+				centerPadding: '30px',
 			}
 		},
 		{
 			breakpoint: 380,
 			settings: {
+				dots: false,
 				slidesToShow: 1,
 				slidesToScroll: 1
 			}
